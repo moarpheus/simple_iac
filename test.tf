dd52bc5f0a550747fbec4f5c914d0a9fcf1523bd
JG <juris.gribuska@bt.com>
2021-08-29 14:49:44 +0300

    updated gitignore and added resourcs to test

--- test.tf
+++ test.tf
@@ -44,3 +44,43 @@ data "aws_ami" "aws-linux" {
   }
 }
 
+##################################################################################
+# RESOURCES
+##################################################################################
+
+resource "aws_default_vpc" "default" {
+
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "test_sg"
+  description = "Allow ports for test"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+##################################################################################
+# OUTPUT
+##################################################################################
+
+output "aws_instance_public_dns" {
+  value = aws_instance.nginx.public_dns
+}
