2bdaa34537c51e5ade25db3cbeb5217c3df168f1
JG <juris.gribuska@bt.com>
2021-08-30 14:56:26 +0300

    added data routing in/out of vpc

--- test.tf
+++ test.tf
@@ -55,14 +55,45 @@ data "aws_ami" "aws-linux" {
 # RESOURCES
 ##################################################################################
 
-resource "aws_default_vpc" "default" {
+# NETWORKING #
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.network_address_space
+  enable_dns_hostnames = "true"
 
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+}
+
+resource "aws_subnet" "subnet1" {
+  cidr_block              = var.subnet1_address_space
+  vpc_id                  = aws_vpc.vpc.id
+  map_public_ip_on_launch = "true"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+
+}
+
+# ROUTING #
+resource "aws_route_table" "rtb" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta-subnet1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rtb.id
 }
 
 resource "aws_security_group" "allow_ssh" {
   name        = "test_sg"
   description = "Allow ports for test"
-  vpc_id      = aws_default_vpc.default.id
+  vpc_id      = aws_vpc.vpc.id
 
   ingress {
     from_port   = 22
@@ -84,6 +115,7 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
+# INSTANCES #
 resource "aws_instance" "nginx" {
   ami                    = data.aws_ami.aws-linux.id
   instance_type          = "t2.micro"
