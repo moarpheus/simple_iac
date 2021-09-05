a74e14e74ba6f3111ddee8f523b635fde307bca5
JG <juris.gribuska@bt.com>
2021-09-05 14:49:27 +0300

    local dynamic vars

--- test.tf
+++ test.tf
@@ -23,6 +23,19 @@ variable "billing_code_tag" {}
 variable "environment_tag" {}
 
 ##################################################################################
+# LOCALS
+##################################################################################
+
+locals {
+  common_tags = {
+    BillingCode = var.billing_code_tag
+    Environment = var.environment_tag
+  }
+
+  s3_bucket_name = "${var.bucket_name_prefix}-${var.environment_tag}-${random_integer.rand.result}"
+}
+
+##################################################################################
 # PROVIDERS
 ##################################################################################
 
