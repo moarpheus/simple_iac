6b09de6bf7833213eb43c245bc0f3d3481b9f330
JG <juris.gribuska@bt.com>
2021-09-09 11:19:56 +0300

    simple code to iterate and create 2 subnets

--- test.tf
+++ test.tf
@@ -12,27 +12,9 @@ variable "region" {
 variable "network_address_space" {
   default = "10.1.0.0/16"
 }
-variable "subnet1_address_space" {
-  default = "10.1.0.0/24"
-}
-variable "subnet2_address_space" {
-  default = "10.1.1.0/24"
-}
-variable "bucket_name_prefix" {}
-variable "billing_code_tag" {}
-variable "environment_tag" {}
 
-##################################################################################
-# LOCALS
-##################################################################################
-
-locals {
-  common_tags = {
-    BillingCode = var.billing_code_tag
-    Environment = var.environment_tag
-  }
-
-  s3_bucket_name = "${var.bucket_name_prefix}-${var.environment_tag}-${random_integer.rand.result}"
+variable "subnet_count" {
+  default = 2
 }
 
 ##################################################################################
@@ -48,298 +30,24 @@ provider "aws" {
 ##################################################################################
 # DATA
 ##################################################################################
+
 data "aws_availability_zones" "available" {}
-
-data "aws_ami" "aws-linux" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn-ami-hvm*"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
 
 ##################################################################################
 # RESOURCES
 ##################################################################################
 
-#Random ID
-resource "random_integer" "rand" {
-  min = 10000
-  max = 99999
-}
-
 # NETWORKING #
 resource "aws_vpc" "vpc" {
-  cidr_block           = var.network_address_space
-  enable_dns_hostnames = "true"
-
-  tags = merge(local.common_tags, { Name = "${var.environment_tag}-vpc" })
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
+  cidr_block = var.network_address_space
 
 }
 
-resource "aws_subnet" "subnet1" {
-  cidr_block              = var.subnet1_address_space
+resource "aws_subnet" "subnet" {
+  count                   = var.subnet_count
+  cidr_block              = cidrsubnet(var.network_address_space, 8, count.index)
   vpc_id                  = aws_vpc.vpc.id
-  map_public_ip_on_launch = "true"
-  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
 
-}
-
-resource "aws_subnet" "subnet2" {
-  cidr_block              = var.subnet2_address_space
-  vpc_id                  = aws_vpc.vpc.id
-  map_public_ip_on_launch = "true"
-  availability_zone       = data.aws_availability_zones.available.names[1]
-
-}
-
-# ROUTING #
-resource "aws_route_table" "rtb" {
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-}
-
-resource "aws_route_table_association" "rta-subnet1" {
-  subnet_id      = aws_subnet.subnet1.id
-  route_table_id = aws_route_table.rtb.id
-}
-
-resource "aws_route_table_association" "rta-subnet2" {
-  subnet_id      = aws_subnet.subnet2.id
-  route_table_id = aws_route_table.rtb.id
-}
-
-
-resource "aws_security_group" "test-sg" {
-  name        = "test_sg"
-  description = "Allow ports for test"
-  vpc_id      = aws_vpc.vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "elb-sg" {
-  name   = "test-lb_sg"
-  vpc_id = aws_vpc.vpc.id
-
-  #Allow HTTP from anywhere
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  #allow all outbound
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# INSTANCES #
-resource "aws_instance" "test1" {
-  ami                    = data.aws_ami.aws-linux.id
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.subnet1.id
-  vpc_security_group_ids = [aws_security_group.test-sg.id]
-  key_name               = var.key_name
-  iam_instance_profile   = aws_iam_instance_profile.test_profile.name
-
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ec2-user"
-    private_key = file(var.private_key_path)
-
-  }
-
-  provisioner "file" {
-    content = <<EOF
-access_key =
-secret_key =
-security_token =
-use_https = True
-bucket_location = EU
-
-EOF
-    destination = "/home/ec2-user/.s3cfg"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum install nginx -y",
-      "sudo service nginx start",
-      "sudo cp /home/ec2-user/.s3cfg /root/.s3cfg",
-      "sudo pip install s3cmd",
-      "s3cmd get s3://${aws_s3_bucket.web_bucket.id}/website/index.html ."
-      
-    ]
-  }
-}
-
-resource "aws_instance" "test2" {
-  ami                    = data.aws_ami.aws-linux.id
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.subnet2.id
-  vpc_security_group_ids = [aws_security_group.test-sg.id]
-  key_name               = var.key_name
-  iam_instance_profile   = aws_iam_instance_profile.test_profile.name
-
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ec2-user"
-    private_key = file(var.private_key_path)
-
-  }
-
-  provisioner "file" {
-    content = <<EOF
-access_key =
-secret_key =
-security_token =
-use_https = True
-bucket_location = US
-
-EOF
-    destination = "/home/ec2-user/.s3cfg"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum install nginx -y",
-      "sudo service nginx start",
-      "sudo cp /home/ec2-user/.s3cfg /root/.s3cfg",
-      "sudo pip install s3cmd",
-      "s3cmd get s3://${aws_s3_bucket.web_bucket.id}/website/index.html ."
-    ]
-  }
-}
-
-# S3 Bucket config#
-resource "aws_iam_role" "allow_test_s3" {
-  name = "allow_test_s3"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_instance_profile" "test_profile" {
-  name = "test_profile"
-  role = aws_iam_role.allow_test_s3.name
-}
-
-resource "aws_iam_role_policy" "allow_s3_all" {
-  name = "allow_s3_all"
-  role = aws_iam_role.allow_test_s3.name
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:*"
-      ],
-      "Effect": "Allow",
-      "Resource": [
-                "arn:aws:s3:::${local.s3_bucket_name}",
-                "arn:aws:s3:::${local.s3_bucket_name}/*"
-            ]
-    }
-  ]
-}
-EOF
-
-  }
-
-  resource "aws_s3_bucket" "web_bucket" {
-    bucket        = local.s3_bucket_name
-    acl           = "private"
-    force_destroy = true
-
-  }
-
-  resource "aws_s3_bucket_object" "website" {
-    bucket = aws_s3_bucket.web_bucket.bucket
-    key = "/website/index.html"
-    source = "./index.html"
-
-  }
-
-# LOAD BALANCER #
-resource "aws_elb" "lb" {
-  name = "test-lb"
-
-  subnets         = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
-  security_groups = [aws_security_group.elb-sg.id]
-  instances       = [aws_instance.test1.id, aws_instance.test2.id]
-
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
-}
-
-#################################################################################
-# OUTPUT
-#################################################################################
-
-output "aws_instance_public_dns" {
-  value = aws_elb.lb.dns_name
-}
+ }
